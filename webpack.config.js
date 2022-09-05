f36a0f8d0f1c6e31f146e7207467249c024c50fd
Onyx Ganda <onyxganda@gmail.com>
2022-09-05 17:35:54 -0400

    Project init

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// require('dotenv').config({ path: './.env.local' }); 
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "docs"),
+  },
+  devServer: {
+    host: "localhost",
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader","css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader","css-loader", "sass-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader","css-loader",
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                  javascriptEnabled: true,
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.css', ".tsx", ".ts"
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
