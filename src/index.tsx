afe3738bdf574359690a3c72a1ef6b02638fb7f2
Onyx Ganda <onyxganda@gmail.com>
2022-09-07 12:28:48 -0400

    Add App's base structure

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './views/App';
+import './styles/index.scss';
+import { HashRouter as Router } from 'react-router-dom'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <div id="app">
+                <Router><App/></Router>
+            </div>
+        </Provider>
+    </React.StrictMode>, document.getElementById('root')
+);
