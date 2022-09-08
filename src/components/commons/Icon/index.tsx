7be07a018d8b14a4ba8463bcc8d14b5b3890ac0f
Onyx Ganda <onyxganda@gmail.com>
2022-09-08 09:56:50 -0400

    Added Icon component

---
+++ src/components/commons/Icon/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../../styles/icomoon.css';
+// import '~styles/icomoon.css';
+
+interface IconProps {
+    name: string
+}
+const Icon = ( props: IconProps ) => {
+    const { name } = props;
+    return(<i className={`icon-${name}`}>
+    </i>)
+}
+
+export default Icon;
