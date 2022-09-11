1605db306dd22697de4341e26b5dedc70c978380
Onyx Ganda <onyxganda@gmail.com>
2022-09-11 18:41:20 -0400

    + content state to store

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import ui, { UIState } from './features/ui';
+import ui, { UIState } from 'features/ui';
+import content, { ContentState } from 'features/content';
 
 export type StoreState = {
     ui: UIState;
+    content: ContentState
 }
 
 export const store = configureStore({
   reducer: {
-    ui
+    ui,
+    content
   },
 });
