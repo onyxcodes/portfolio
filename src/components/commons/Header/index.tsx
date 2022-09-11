12d9b3d971e5aa09cdc8a5d43b2f3270b501e84e
Onyx Ganda <onyxganda@gmail.com>
2022-09-11 18:38:42 -0400

    +Header cmp

---
+++ src/components/commons/Header/index.tsx
@@ -0,0 +1,34 @@
+import React, { forwardRef } from 'react';
+import { useSelector } from 'react-redux';
+import { UIState } from 'features/ui';
+import { StoreState } from 'store';
+import Breadcrumb from 'components/commons/Breadcrumb';
+import './index.scss';
+
+interface HeaderProps {
+    onTitleClick?: (arg: any) => void;
+    title?: string;
+}
+const Header = forwardRef( (props: HeaderProps, ref: React.LegacyRef<HTMLDivElement>) => {
+    const { onTitleClick, title } = props;
+	const path = useSelector<StoreState, UIState['path']>( s => s.ui.path );
+
+    let headerClass = 'header';
+
+    if ( path !== '/' ) headerClass = `${headerClass} extended`;
+
+    return(<div ref={ref} className={headerClass}>
+        <div
+            className='header-title'
+            title={title}
+            onClick={onTitleClick}
+            style={{backgroundImage: `url(${require('assets/logo.svg')})`}}
+        >
+            &nbsp;
+            {/* TODO: add span for accessibility */}
+        </div>
+        <Breadcrumb />
+    </div>)
+})
+
+export default Header
