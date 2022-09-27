e731793dad4b808f6d358baedb3a9779d5806420
Onyx Ganda <onyxganda@gmail.com>
2022-09-27 18:32:02 -0400

    + TextInput component styling and behaviour mgt

--- src/components/commons/Form/TextInput/index.tsx
+++ src/components/commons/Form/TextInput/index.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import './index.scss';
 
-interface TextInputProps {
-    name: string;
-    label?: string;
-    placeholder?: string;
-    inline?: boolean;
-    labelSeparator?: string;
-    required?: boolean;
-    onChange?: ( arg?: string | null) => void;
-    /* Method used to perform validation:
-     * returns true or an error message when invalid
-     * false when the field is valid!
-     */
-    validator?: ( arg?: string | null ) => boolean | string;
+import { InputProps, InputRefType } from 'components/commons/Form/types'
+
+export interface TextInputProps extends InputProps {
+    // TODO: Extend with other compatible types
+    type: 'text' | 'email';
 }
-const TextInput = React.forwardRef(( props: TextInputProps, ref ) => {
+const TextInput = React.forwardRef( ( props: TextInputProps, ref: React.ForwardedRef<InputRefType> ) => {
     const { 
         name,
         label,
+        type,
         onChange,
         validator,
         required = false,
@@ -36,25 +29,31 @@ const TextInput = React.forwardRef(( props: TextInputProps, ref ) => {
      * a method to trigger the field validation
      */
     React.useImperativeHandle(ref, () => ({
+        isInputRefType: true,
         checkValidity,
         getValidity: () => isInvalid,
         current: inputRef.current
     }), [isInvalid]);
 
-    let inputClass = 'input-text f';
-    if ( inline ) inputClass = `${inputClass} fd-row`;
-    else inputClass = `${inputClass} fd-col`;
+    let inputClass = 'input-text f',
+        inputWrapperClass = 'input-wrapper-class m05 f';
+
+    if ( inline ) inputWrapperClass = `${inputWrapperClass} fd-row`;
+    else inputWrapperClass = `${inputWrapperClass} fd-col`;
 
     if ( required ) inputClass = `${inputClass} input-required`;
 
     if ( isInvalid.length ) inputClass = `${inputClass} input-invalid`;
+
+    // Based on size assign classes
+    inputClass = `${inputClass} col-4 col-lg-8 col-sm-12`;
 
     const checkValidity = React.useCallback( () => {
         const value = inputRef?.current?.value;
         let errorMessages = [];
         // If provided, perform validator method
         if (validator) {
-            let result = validator(value);
+            let result = validator(value!);
             // When the validator returns true or message
             // is invalid
             if (result) errorMessages.push(result);
@@ -62,30 +61,35 @@ const TextInput = React.forwardRef(( props: TextInputProps, ref ) => {
         // When field is required and is missing value, add the error
         if (required && !value) errorMessages.push('This field is mandatory');
         markInvalid(errorMessages);
+        return errorMessages;
     }, [inputRef.current, validator, required]);
 
     const onValueChange = React.useCallback( () => {
         const value = inputRef?.current?.value;
         onChange && onChange(value);
+        checkValidity();
     }, [onChange, required]);
 
+    /* Transforms 'isInvalid' array of errors into a list
+     */
     const renderedErrors = React.useMemo( () => isInvalid.map( (err, i) => 
         <li key={i}>{ typeof err === 'string' ? err : 'Check this field' }</li>
-    )
-    , [isInvalid])
+    ), [isInvalid]);
 
     return <div className={inputClass}>
-        { label && 
-            <label className='input-text-label' htmlFor={name}>{`${label}${labelSeparator}`}</label>
-        }
-        <input ref={inputRef} 
-            type='text'
-            onChange={onValueChange}
-            placeholder={placeholder}
-        />
-        { isInvalid.length && <ul className='input-errors'>
-            { renderedErrors }
-        </ul>}
+        <div className={inputWrapperClass}>
+            { label && 
+                <label className='input-text-label' htmlFor={name}>{`${label}${labelSeparator}`}</label>
+            }
+            <input ref={inputRef} 
+                type={type}
+                onChange={onValueChange}
+                placeholder={placeholder}
+            />
+            { isInvalid.length ? <ul className='input-errors my05 t6'>
+                { renderedErrors }
+            </ul> : <></>}
+        </div>
     </div>
 });
 
