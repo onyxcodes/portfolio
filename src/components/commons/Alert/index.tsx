1eea0a241dd12215b08b65b6acbe502c164e2b95
Onyx Ganda <onyxganda@gmail.com>
2022-09-28 15:29:43 -0400

    + Alert component

---
+++ src/components/commons/Alert/index.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import './index.scss';
+import Icon from 'components/commons/Icon';
+import Button from 'components/commons/Button';
+import { NotificationType } from 'features/ui/types';
+
+interface AlertProps extends NotificationType {
+    order?: number;
+    showIcon?: boolean;
+}
+const Alert = ( props: AlertProps ) => {
+    const {
+        id, order = 0,
+        level = 'info',
+        message, actions,
+        clearable = true,
+        timeout,
+        timestamp,
+        showIcon = true,
+    } = props;
+    
+    const [ visible, setVisible ] = React.useState(true);
+    const [ mounted, mount ] = React.useState(false);
+    const [ visibility, setVisibility ] = React.useState(false);
+
+    let alertClass = `alert alert-${level}`;
+
+    React.useLayoutEffect( () => {
+        let unmountTimeoutId: number,
+            visiblityTimeoutId: number;
+        if ( visible ) {
+            mount(visible);
+            visiblityTimeoutId = window.setTimeout( () => setVisibility(true), 150)
+        } else {
+            unmountTimeoutId = window.setTimeout( () => mount(visible), 1000);
+            setVisibility(visible);
+        }
+    }, [visible]);
+
+    if ( visibility ) {
+        alertClass = `${alertClass} visible`;
+    }
+
+    let alertIcon;
+    if ( showIcon ) {
+        let iconName;
+        switch (level) {
+            case 'info':
+                iconName = 'info-circle';
+            break;
+            case 'warning':
+                iconName = 'exclamation-triangle';
+            break;
+            case 'debug':
+                iconName = 'lightbulb-o';
+            break;
+            case 'error':
+                iconName = 'frown-o';
+            break;
+            case 'prompt':
+                iconName = 'question-circle'
+            break;
+        }
+        alertIcon = <div className='alert-icon f aic p05'>
+            <Icon name={iconName} />
+        </div>
+    }
+
+    return mounted ? <>
+        <div id={`alert-${order}`} className={alertClass}>
+            { clearable && <Button onClick={() => setVisible(false)} className='f-right m025' 
+                iconName='close' shape='circle' type='text'
+            /> }
+            <div className='alert-wrapper f fd-row p05'>
+                { alertIcon }
+                <div className='alert-content f aic t6 px05'>
+                    <span>{message}</span>
+                </div>
+            </div>
+        </div>
+    </> : <></>
+}
+
+export default Alert;
