90d6e817ca7519bf27231ae7e5af6790465426ea
Onyx Ganda <onyxganda@gmail.com>
2022-09-29 22:30:57 -0400

    + Contact api management of failures

--- src/features/content/custom/contactform.ts
+++ src/features/content/custom/contactform.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getStrapiURL } from 'utils/strapi';
+import notify from 'features/ui/notify'
+import { NotificationType } from 'features/ui';
 
 const contactSend = async ( payload: {} ) => {
     const { data } = await axios.post(
@@ -17,7 +19,21 @@ const contactSend = async ( payload: {} ) => {
 
 const sendContactInquiry = createAsyncThunk('sendContactInquiry',
     async ( arg: {}, thunkApi ) => {
+        const { dispatch, rejectWithValue } = thunkApi;
         let response = await contactSend(arg);
+        if (!response.success) {
+            throw rejectWithValue({
+                success: false,
+                error: 'Problem while communicating with api server'
+            }); 
+        }
+        let notification: NotificationType = {
+            id: thunkApi.requestId,
+            level: 'info',
+            message: 'Your inquiry was successfully submitted.',
+            clearable: true
+        }
+        dispatch(notify(notification));
         return response;
     }
 );
